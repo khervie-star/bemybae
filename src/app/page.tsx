d43916680c3de30041858a46c2f7034445edd4b1
Kwesi Hervie <herviek2001@gmail.com>
2024-01-24 15:23:27 +0100

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <main className="flex w-full h-screen p-5 lg:p-8 bg-[#f8c8dc]">
       <div className="w-full h-full flex justify-center items-center">
         <div className="w-full lg:max-w-[800px]">
-          <h1 className="text-[20px] lg:text-[28px] font-bold text-gray-900 mb-8 font-lato text-center">
+          <h1 className="text-[20px] lg:text-[28px] font-bold text-gray-700 mb-8 font-snowtimes text-center">
             Heyy Angelface, 🥺🥺🥺🥺
           </h1>
-          <p className="text-[40px] text-pink-600 lg:text-[70px] font-semibold font-snowtimes text-center">
+          <p className="text-[50px] text-pink-600 lg:text-[80px] font-semibold font-snowtimes text-center">
             Will you be my valentine? 🌸🩷
           </p>
         </div>
